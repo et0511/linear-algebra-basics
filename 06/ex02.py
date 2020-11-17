2342bb8e3405928725912fdf8d2d926972abcb6f
et0511 <shinwon0511@gmail.com>
2020-11-17 17:22:07 +0900

    미분 #1

---
+++ 06/ex02.py
@@ -0,0 +1,16 @@
+# 수치미분(Numerical Diffirentiation VS 해석미분(Analytic Diffirenriarion)
+# 이차함수 y=20*(x-2)^2 + 500
+
+def f(x):
+    return 20*(x-2)**2+500
+
+def analytic_diff(x):
+    return 40 * x - 80
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x)) / h
+
+
+print(f'Numerical Diffirentation Value:{numerical_diff(f, 5)}')
+print(f'Analytic Diffirentation Value:{analytic_diff(5)}')
---
+++ 07/01.py
@@ -0,0 +1,15 @@
+# 편미분(partial Diffirentiation)
+
+def f(x0):
+    return x0**2 + 4**2
+
+def numerical_diff(f, x0):
+    h = 1.0e-4
+    return (f(x0 + h) - f(x0)) / h
+
+def analytic_diff(x0):
+    return 2 * x0
+
+print(f'Numerical Diffirentiation Value:{numerical_diff(f, 3)}')
+print(f'(Analytic Diffirentiation Value:{analytic_diff(3)}')
+
