380c7c81e851a9de6c63819a6e8f7019b2bc83fc
et0511 <shinwon0511@gmail.com>
2020-11-18 11:49:20 +0900

    numerical gradient 구하기

---
+++ 07/02.py
@@ -0,0 +1,24 @@
+# 편미분(partial Diffirentiation)
+# x0 = 3, x1 = 4
+
+def f(x1):
+    return 3.**2 + x1**2
+
+def numerical_diff(f, x1):
+    h = 1.0e-4      # 개인 지정
+    return (f(x1 + h) - f(x1 - h)) / (2 * h)
+
+def analytic_diff(x1):
+    return 2 * x1
+
+# (x0, x1) = (3, 4)
+print(f'Numerical Diffirentiation Value:{numerical_diff(f, 4)}')
+print(f'(Analytic Diffirentiation Value:{analytic_diff(4)}')
+
+# (x0, x1) = (3, 1)
+print(f'Numerical Diffirentiation Value:{numerical_diff(f, 1)}')
+print(f'(Analytic Diffirentiation Value:{analytic_diff(1)}')
+
+# (x0, x1) = (3, 2)
+print(f'Numerical Diffirentiation Value:{numerical_diff(f, 2)}')
+print(f'(Analytic Diffirentiation Value:{analytic_diff(2)}')
---
+++ 08/ex01_gradient.py
@@ -0,0 +1,39 @@
+# 기울기(gradient)
+import numpy as np
+
+def f(x):
+    return np.sum(x**2, axis=0)
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    gradient = np.zeros_like(x)
+
+    # h1 = f(np.array([x[0] + h, x[1]]))
+    # h2 = f(np.array([x[0] - h, x[1]]))
+    # gradient[0] = (h1 - h2) / (2 * h)
+
+    # h1 = f(np.array([x[0], x[1] + h]))
+    # h2 = f(np.array([x[0], x[1] - h]))
+    # gradient[1] = (h1 - h2) / (2 * h)
+    for i in range(x.size):
+        tmp = x[i]
+
+        x[i] = tmp + h
+        h1 = f(x)
+
+        x[i] = tmp - h
+        h2 = f(x)
+
+        gradient[i] = (h1 - h2) / (2 * h)
+        x[i] = tmp
+
+    return gradient
+
+# 함수 테스트
+# print(f(np.array([3., 4.])))
+
+gra1 = numerical_gradient(f, np.array([3, 4.]))
+gra2 = numerical_gradient(f, np.array([0, 2.]))
+gra3 = numerical_gradient(f, np.array([3, 0.]))
+
+print(gra1, gra2, gra3)
