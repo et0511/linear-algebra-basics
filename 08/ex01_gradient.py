f7d6c7d403c144c169223f72ff5d79e4803f51dc
et0511 <shinwon0511@gmail.com>
2020-11-18 14:22:16 +0900

    common 모듈만들기

--- 08/ex01_gradient.py
+++ 08/ex01_gradient.py
@@ -1,37 +1,21 @@
 # 기울기(gradient)
+import os
+import sys
+from pathlib import Path
 import numpy as np
+
+try:
+    sys.path.append(os.path.join(Path(os.getcwd()).parent, 'lib'))
+    from common import numerical_gradient
+
+except ImportError:
+    print('Library Module Can Not Found')
 
 def f(x):
     return np.sum(x**2, axis=0)     # 손실 함수
 
-def numerical_gradient(f, x):
-    h = 1e-4
-    gradient = np.zeros_like(x)
-
-    # h1 = f(np.array([x[0] + h, x[1]]))
-    # h2 = f(np.array([x[0] - h, x[1]]))
-    # gradient[0] = (h1 - h2) / (2 * h)
-
-    # h1 = f(np.array([x[0], x[1] + h]))
-    # h2 = f(np.array([x[0], x[1] - h]))
-    # gradient[1] = (h1 - h2) / (2 * h)
-    for i in range(x.size):
-        tmp = x[i]
-
-        x[i] = tmp + h
-        h1 = f(x)
-
-        x[i] = tmp - h
-        h2 = f(x)
-
-        gradient[i] = (h1 - h2) / (2 * h)
-        x[i] = tmp
-
-    return gradient
-
 # 함수 테스트
 # print(f(np.array([3., 4.])))
-
 gra1 = numerical_gradient(f, np.array([3, 4.]))
 gra2 = numerical_gradient(f, np.array([-1, -1.5]))
 gra3 = numerical_gradient(f, np.array([-0.25, -0.25]))
