6b68d3bead42774416902e12e2cd4f1ec4b086f1
et0511 <shinwon0511@gmail.com>
2020-11-18 10:41:29 +0900

    미분 #1

--- 07/01.py
+++ 07/01.py
@@ -6,11 +6,19 @@ def f(x0):
 
 def numerical_diff(f, x0):
     h = 1.0e-4      # 개인 지정
-    return (f(x0 + h) - f(x0)) / h
+    return (f(x0 + h) - f(x0 - h)) / (2 * h)
 
 def analytic_diff(x0):
     return 2 * x0
 
+# (x0, x1) = (3, 4)
 print(f'Numerical Diffirentiation Value:{numerical_diff(f, 3)}')
 print(f'(Analytic Diffirentiation Value:{analytic_diff(3)}')
 
+# (x0, x1) = (1, 4)
+print(f'Numerical Diffirentiation Value:{numerical_diff(f, 1)}')
+print(f'(Analytic Diffirentiation Value:{analytic_diff(1)}')
+
+# (x0, x1) = (2, 4)
+print(f'Numerical Diffirentiation Value:{numerical_diff(f, 2)}')
+print(f'(Analytic Diffirentiation Value:{analytic_diff(2)}')
