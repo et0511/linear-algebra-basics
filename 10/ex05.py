c1306e4361e6002bc5e4aa43f017d6494b40d4a1
et0511 <shinwon0511@gmail.com>
2020-11-20 11:40:25 +0900

    다중선형회귀 연습#2

--- 10/ex05.py
+++ 10/ex05.py
@@ -21,10 +21,36 @@ def mean_squares_error(x, data_training):
     return e
 
 
+# <경사하강법 연습>
 # data
 times = [2, 4, 6, 8]
 ptimes = [0, 4, 2, 3]
 scores = [81, 93, 91, 97]
+
+
+x = np.array([0, 0, 0])
+for i in range(5000):
+    h = 1.0e-4
+    h1 = mean_squares_error(np.array([x[0]+h, x[1], x[2]]), (times, ptimes, scores))
+    h2 = mean_squares_error(np.array([x[0]-h, x[1], x[2]]), (times, ptimes, scores))
+    dx0 = (h1-h2) / (2*h)
+
+    h1 = mean_squares_error(np.array([x[0], x[1]+h, x[2]]), (times, ptimes, scores))
+    h2 = mean_squares_error(np.array([x[0], x[1]-h, x[2]]), (times, ptimes, scores))
+    dx1 = (h1-h2)/ (2 * h)
+
+    h1 = mean_squares_error(np.array([x[0], x[1], x[2] + h]), (times, ptimes, scores))
+    h2 = mean_squares_error(np.array([x[0], x[1], x[2] - h]), (times, ptimes, scores))
+    dx2 = (h1-h2) / (2 * h)
+
+    gradient = np.array([dx0, dx1, dx2])
+    print(f'epoch={i + 1}, gradient={gradient}, x={x}')
+
+    x = x - 0.01 * gradient
+
+result = x
+
+
 
 # 경사하강법
 result = gradient_descent(mean_squares_error, np.array([0., 0., 0.]), epoch=3000, data_training=(times, ptimes, scores))
